e25e897402df1187730c307161b07b0677258754
Mathieu Deous <mat.deous@gmail.com>
2013-04-01 02:15:17 +0200

    display event details in a modal when clicked

--- eventviz/views/timeline.py
+++ eventviz/views/timeline.py
@@ -26,18 +26,24 @@ def index():
         if 'group' in request.form:
             group = request.form['group']
     data = []
+    events = []
     for event_type in get_event_types(project):
         for db_item in db[event_type].find():
+            db_item['_id'] = str(db_item['_id'])
+            db_item['time'] = db_item['time'].strftime(settings.JS_DATE_FORMAT)
             item = {
-                'start': db_item['time'].strftime(settings.JS_DATE_FORMAT),
+                'start': db_item['time'],
                 'group': db_item.get(group, 'N/A') if group is not None else event_type,
-                'content': ' - '.join(map(lambda f: str(db_item.get(f, 'N/A')), displayed_fields))
+                'content': ' - '.join(map(lambda f: str(db_item.get(f, 'N/A')), displayed_fields)),
+                'className': db_item['_id']
             }
             data.append(item)
+            events.append(db_item)
     return render_template(
         'timeline.html',
         page='timeline',
         project=project,
         event_fields=available_fields,
-        data=data
+        data=data,
+        events=events
     )
