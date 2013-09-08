6dcd1ae5c17d26c3f676ef1b7aca06d7e715df14
Mathieu Deous <mat.deous@gmail.com>
2013-09-08 17:10:54 +0200

    display timeline filters - related to #9

--- eventviz/views/timeline.py
+++ eventviz/views/timeline.py
@@ -25,6 +25,8 @@ def index():
             displayed_fields = form_fields
         if 'group' in request.form:
             group = request.form['group']
+            if group == 'event_type':
+                group = None
     data = []
     events = []
     for event_type in get_event_types(project):
@@ -32,19 +34,25 @@ def index():
             db_item_id = str(db_item['_id'])
             item = {
                 'start': db_item['time'].strftime(settings.JS_DATE_FORMAT),
-                'group': db_item.get(group, 'N/A') if group is not None else event_type,
+                'group': db_item.get(group, 'E_NOGROUP') if group is not None else event_type,
                 'content': ' - '.join(map(lambda f: str(db_item.get(f, 'N/A')), displayed_fields)),
                 'className': '%s eventtype-%s' % (db_item_id, event_type)
             }
             data.append(item)
             events.append(db_item_id)
+    events.append('event_type')
+    filters = {
+        'fields': ','.join(displayed_fields),
+        'group_by': group or 'event_type'
+    }
     return render_template(
         'timeline.html',
         page='timeline',
         project=project,
         event_fields=available_fields,
         data=data,
-        events=events
+        events=events,
+        filters=filters
     )
 
 @timeline.route('/<string:event_type>/<string:event_id>')
